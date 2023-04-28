2814c662990f627bde3a1d1ae99a813d9679560a
zero4573 <andya.to@gmail.com>
2023-04-28 19:48:09 +0000

    Deploying to gh-pages from @ zero4573/website@660b1bd453c36e985777c979358430dd061d4d37 🚀

--- _nuxt/products.89e0891b.js
+++
@@ -1,1 +0,0 @@
-import{bG as s}from"./entry.e5bb01c1.js";const e=[{id:1,name:"Sunglasses",price:10.99,image:"sunglasses.jpg"},{id:2,name:"Shoes",price:99.01,image:"shoes.jpg"}],o=s("products-store",{state:()=>({products:[]}),getters:{},actions:{async retrieveProducts(){this.products=e}}});export{o as u};
